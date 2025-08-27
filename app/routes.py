1353389a880c37dc6fe53a03c083600161c0f263
Uduak Umanah <eyakenojnr@gmail.com>
2025-08-27 17:41:24 +0100

    receive form data and added `/home` route

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,24 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    """Home page.
+    """Welcome page.
     """
     #user = {'username': 'taddyP'}
-    return render_template('index.html')
+
+    # Receive login credentials
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect(url_for('home'))
+
+    return render_template('index.html', title='Welcome Page', form=form)
+
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
