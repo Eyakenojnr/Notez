8707d4c8ee52cbefd034ac6302cfc2f27d93647a
Uduak Umanah <eyakenojnr@gmail.com>
2025-08-27 09:33:55 +0100

    Read and apply `config` file

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from config import Config
 
 
 app = Flask(__name__)
+
+# Read and apply configurations from the config file
+app.config.from_object(Config)
 
 from app import routes
