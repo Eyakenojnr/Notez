d6a1edc06cadce1315cfcfe3002b8fecbca7823e
Uduak Umanah <eyakenojnr@gmail.com>
2025-08-30 22:51:44 +0100

    db uri

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 """Configuration file."""
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
     SECRET_KEY = os.environ.get('MY_KEY') or 'a-b-c-d'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
