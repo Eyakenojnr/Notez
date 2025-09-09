6aceac2eff5ca4fcf9d74d0d1cb08c8012aef8a3
Uduak Umanah <eyakenojnr@gmail.com>
2025-09-09 14:35:34 +0100

    Password hashing and verification

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
 from datetime import datetime, timezone
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 # --- Association Tables for Many-to-Many Relationships ---
@@ -55,6 +56,12 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>'
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
     
 
 class Note(db.Model):
