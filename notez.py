c1ba06a6dd191bb37fc379bcf4e80a58df50d1d0
Uduak Umanah <eyakenojnr@gmail.com>
2025-09-09 14:39:46 +0100

    Add shell context to shell session

--- notez.py
+++ notez.py
@@ -1,2 +1,15 @@
-"""Main application module"""
-from app import app
+"""Main application module.
+"""
+from app import app, db
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app.models import User, Note, Group, ToDoList, ToDoItem, Tag
+
+
+@app.shell_context_processor
+def make_shell_context():
+    """Create shell context that adds the database instance and models to
+    the shell session.
+    """
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Note': Note,
+            'Group': Group, 'ToDoList': ToDoList, 'ToDoItem': ToDoItem, 'Tag': Tag}
