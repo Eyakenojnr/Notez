3a92f3e463a9b65d90e2b211bda5125e9314d68c
Uduak Umanah <eyakenojnr@gmail.com>
2025-09-09 14:38:42 +0100

    Configure Flask-SQLAlchemy to log all its queries to the console

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ class Config:
     SECRET_KEY = os.environ.get('MY_KEY') or 'a-b-c-d'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_ECHO = True
