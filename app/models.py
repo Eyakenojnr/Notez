0688ed983e5e00dd428954e5054c6e4b6c72889c
Uduak Umanah <eyakenojnr@gmail.com>
2025-09-10 16:18:47 +0100

    Loader function

--- app/models.py
+++ app/models.py
@@ -3,9 +3,10 @@
 from typing import List, Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
+from app import db, login
 from datetime import datetime, timezone
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
 # --- Association Tables for Many-to-Many Relationships ---
@@ -25,8 +26,13 @@ todolist_tag_association = sa.Table(
 
 
 # --- Main Model Classes ---
-class User(db.Model):
+class User(UserMixin, db.Model):
     """User database model.
+
+    Args:
+        UserMixin (class): Class that includes safe implementations of flask_login 
+            requirements for user model classes.
+        db (class): Base class.
     """
 
     __tablename__ = 'user'
@@ -207,3 +213,16 @@ class Tag(db.Model):
     
     def __repr__(self):
         return f'<Tag {self.name}>'
+    
+
+@login.user_loader
+def load_user(id):
+    """User loader function.
+
+    Args:
+        id (string): ID of user to be loaded.
+
+    Returns:
+        object: Returns a User object or None.
+    """
+    return db.session.get(User, int(id))
