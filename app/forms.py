c3fdc573088c63c7bb79059ee2e34e7941f1f2cf
Uduak Umanah <eyakenojnr@gmail.com>
2025-09-11 17:42:40 +0100

    Signup form

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,10 @@
 """Web forms"""
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+import sqlalchemy as sa
+from app import db
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -12,3 +15,28 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired('Please enter a password')])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class SignupForm(FlaskForm):
+    """User registration form.
+    """
+
+    first_name = StringField('Firstname', validators=[DataRequired('Must provide first name.')])
+    last_name = StringField('Lastname')
+    username = StringField('Username', validators=[DataRequired()]) # validation for blank, too short, too long inputs
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = StringField('Password', validators=[DataRequired()])
+    password2 = StringField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        """Custom validator to validate username input...
+        """
+        user = db.session.scalar(sa.select(User).where(User.username == username.data))
+        if user is not None:
+            raise ValidationError('Use a different username, this is taken!')
+        
+    def validate_email(self, email):
+        user = db.session.scalar(sa.select(User).where(User.email == email.data))
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
